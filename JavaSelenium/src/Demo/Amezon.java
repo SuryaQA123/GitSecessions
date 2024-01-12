5673484201df415e0dd05edbbbfa62c2dc9324d9
SuryaQA123 <devendraAutomation0622@gmail.com>
2024-01-12 15:11:06 +0530

    Createing methods and  calling

--- JavaSelenium/src/Demo/Amezon.java
+++ JavaSelenium/src/Demo/Amezon.java
@@ -2,13 +2,18 @@ package Demo;
 
 public class Amezon {
 	public static void main(String[] args) {
-		
+		funA();
+		funC();
 	}
 public void funB() {
 	System.out.println("Fun B");
 }
 	public static void funA() {
 		
+	}
+	
+	public static  void funC() {
+		System.out.println("Fun C invoked Successfully");
 	}
 
 
