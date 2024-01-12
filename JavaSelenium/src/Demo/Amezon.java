43bd625877565c2b0d558aba4bcbc4f2853bf096
SuryaQA123 <devendraAutomation0622@gmail.com>
2024-01-12 16:55:26 +0530

    master commit

--- JavaSelenium/src/Demo/Amezon.java
+++ JavaSelenium/src/Demo/Amezon.java
@@ -2,24 +2,27 @@ package Demo;
 
 public class Amezon {
 	public static void main(String[] args) {
-System.out.println("this is from GIT X");
+		System.out.println("this is from GIT X");
 		funA();
 		funC();
 		Amezon a1 = new Amezon();
+		a1.funB();
+	}
+
+	public void funB() {
+		System.out.println("Fun B");
+	}
+
+	public static void funA() {
 
 	}
-public void funB() {
-	System.out.println("Fun B");
-}
-	public static void funA() {
-		
-	}
-	
-	public static  void funC() {
+
+	public static void funC() {
 		System.out.println("Fun C invoked Successfully");
 		funA();
 	}
-	public static  void funD() {
+
+	public static void funD() {
 		System.out.println("Fun C invoked Successfully");
 	}
 
